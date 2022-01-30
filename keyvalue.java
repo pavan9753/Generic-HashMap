ddd04177303d3486b516c84e13a18cc3d2cb3f25
Ramagiri Pavan <76248320+pavan9753@users.noreply.github.com>
2022-01-30 22:41:52 +0530

    Add files via upload

---
+++ keyvalue.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+
+class keyval<K,V>
+    {
+    K k;
+    V v;
+    keyval()
+    {
+        
+    }
+    keyval(K k,V v)
+    {
+        this.k=k;
+        this.v=v;
+    }
+   }
